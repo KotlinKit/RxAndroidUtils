427daf2b48c4376b35e09ba7c6b35645a9347877
Matous Hybl <hyblmatous@gmail.com>
2016-10-15 19:18:36 +0200

    Add reactive menu handling.

--- library/src/main/java/org/brightify/rxandroidutils/BaseRxActivity.kt
+++ library/src/main/java/org/brightify/rxandroidutils/BaseRxActivity.kt
@@ -2,6 +2,7 @@ package org.brightify.rxandroidutils
 
 import android.content.Intent
 import android.support.v4.app.Fragment
+import android.view.MenuItem
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
 import rx.Observable
 import rx.lang.kotlin.PublishSubject
@@ -21,6 +22,18 @@ abstract class BaseRxActivity : RxAppCompatActivity() {
 
     protected val rxActivityResult: Observable<ActivityResult>
         get() = rxActivityResultSubject.asObservable()
+
+    private val rxMenuSubject = PublishSubject<MenuItem>()
+
+    val rxMenuItemSelected: Observable<MenuItem>
+        get() = rxMenuSubject
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            rxMenuSubject.onNext(it)
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
--- library/src/main/java/org/brightify/rxandroidutils/BaseRxFragment.kt
+++ library/src/main/java/org/brightify/rxandroidutils/BaseRxFragment.kt
@@ -1,6 +1,7 @@
 package org.brightify.rxandroidutils
 
 import android.content.Intent
+import android.view.MenuItem
 import com.trello.rxlifecycle.components.support.RxFragment
 import rx.Observable
 import rx.lang.kotlin.PublishSubject
@@ -14,6 +15,18 @@ abstract class BaseRxFragment : RxFragment() {
 
     protected val rxActivityResult: Observable<ActivityResult>
         get() = rxActivityResultSubject.asObservable()
+
+    private val rxMenuSubject = PublishSubject<MenuItem>()
+
+    val rxMenuItemSelected: Observable<MenuItem>
+        get() = rxMenuSubject
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            rxMenuSubject.onNext(it)
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
